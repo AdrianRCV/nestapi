88df2cba22f79547e271314eff3effab43d36708
AdrianRCV <adriancamposvargas4@gmail.com>
2025-01-14 14:26:33 +0100

    20250114

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { ProductsController } from './products/products.controller';
-import { AlumnosController } from './products/alumnos/alumnos.controller';
+import { ProductsService } from './products/products.service';
+
+
+
 
 @Module({
-  imports: [ConfigModule.forRoot({isGlobal: true,})],
-  controllers: [ProductsController, AlumnosController],
-  providers: [],
+  imports: [ConfigModule.forRoot({isGlobal:true,})],
+  controllers: [ProductsController],
+  providers: [ProductsService],
 })
 export class AppModule {}
--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Post, Put, Query } from '@nestjs/common';
+import { stat } from 'fs';
+import { ProductsService } from './products.service';
 
+interface datos {id?:string,size?:string};
+type Product={articulo:string,precio:number,descripcion:string};
 @Controller('products')
 export class ProductsController {
+    constructor(private readonly servicioProducts: ProductsService) {}
     @Get()
     findProducts():string{
         return "Total de productos en el aula 2ASIR"
@@ -13,26 +18,51 @@ export class ProductsController {
     @Get('camiseta/roja')
     findAdios():string{
         return "Total de camisetas rojas!!"; 
-    }    
-    @Get('hot')
-    findHot(){
-        return "Productos calientes!"
+    }   
+    @Get('query')
+    busqueda(@Query() consulta:any):string {
+        return `Estas consultando por el ${consulta.articulo} con el precio ${consulta.precio}`;
     }
-    @Get(':id')
-    findByid(@Param() parametros:any):string{
-        return `Obteniendo productos del parámetro ${parametros.id}`
+    // @Get(':id')
+    // findById(@Param() parametros:any):string{
+    //     return `Obteniendo productso del parámetro ${parametros.id}`
+    // }
+
+    // @Get('hot')
+    // findHot(){
+    //     return "Productos calientes!"
+    // }
+    
+    // @Get(':id/:size')
+    // findByIDSize(@Param() parametros:datos):string{
+    //     return `Obteniendo productos del tipos ${parametros.id} y tamaño ${parametros.size}`
+    // }
+    @Get(':id?/:size?')
+  findWithOptional(@Param() params: datos) {
+    const { id,size } = params;
+    if (size && id) {
+      return `Item ${id} de tamaño ${size}`;
+    } else if (id) {
+      return `Todos los items de tipo ${id}`;
+    } else {
+      return 'Todos los items';
     }
-    @Get(':id/:size')
-    findByIDSize(@Param() parametros:any):string{
-        return `Obteniendo productos del tipo ${parametros.id} y tamaño ${parametros.size}`
-    }
+}
+
+    // @Post()
+    // insertaProducts(@Body('articulo') producto:string,@Body('precio') precio:number ):string{
+    //     return `El producto ${producto} de precio ${precio} se ha insertado correctamente`;
+    // }
     @Post()
-    insertaProducts():string{
-        return "Producto insertado"
+    insertaProducts(@Body() producto:Product){
+        return {
+            status:HttpStatus.OK,
+            message: `El producto ${producto.articulo} de precio ${producto.precio}`
+        };
     }
-    @Put()
-    actualizaProducts():string{
-        return "Producto ACTUALIZADO"
+    @Put(':id')
+    actualizaProducts(@Body() actualizar:any):string{
+        return `El producto tiene que actualizar el articulo a ${actualizar.articulo} y precio a ${actualizar.precio}` 
     }
 
     @Delete()
