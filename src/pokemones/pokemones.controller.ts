ea5b49002c68c15ee5853eaae810e02616f549b2
AdrianRCV <adriancamposvargas4@gmail.com>
2025-01-30 08:57:51 +0100

    20250130

--- src/pokemones/pokemones.controller.ts
+++ src/pokemones/pokemones.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, Put } from '@nestjs/common';
+import { Controller, Get, Post, Body, Query, Param, Delete, Put } from '@nestjs/common';
 import { PokemonesService } from './pokemones.service';
 import { CreatePokemoneDto } from './dto/create-pokemone.dto';
 import { UpdatePokemoneDto } from './dto/update-pokemone.dto';
@@ -18,6 +18,16 @@ export class PokemonesController {
     return this.pokemonesService.findAll();
   }
 
+  @Get('query1')
+  async rutaQuery(@Query('tipo') tipo:string):Promise<Pokemone[]>{
+        return this.pokemonesService.buscaTipo(tipo);
+  }
+
+  @Get('query2')
+  async ruta(@Query('hp') hp:number):Promise<Pokemone[]>{
+        return this.pokemonesService.buscaHP(hp);
+  }
+
   @Get(':id')
   async findOne(@Param('id') id: string):Promise<Pokemone> {
     return this.pokemonesService.findOne(+id);
@@ -29,7 +39,7 @@ export class PokemonesController {
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: string) {
+  async remove(@Param('id') id: string):Promise<string> {
     return this.pokemonesService.remove(+id);
   }
 }
